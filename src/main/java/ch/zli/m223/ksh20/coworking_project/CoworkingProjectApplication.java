b6f2f89c2cd9f4babbcefed80aae4b995c291372
fftop-dev <feroldif@bzz.ch>
2023-07-12 09:01:25 +0200

    package structure

--- src/main/java/ch/zli/m223/ksh20/coworking_project/CoworkingProjectApplication.java
+++ src/main/java/ch/zli/m223/ksh20/coworking_project/CoworkingProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.coworking_project;
+package ch.zli.m223.ksh20.coworking_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ch/zli/m223/ksh20/coworking_project/CoworkingProjectApplicationTests.java
+++ src/test/java/ch/zli/m223/ksh20/coworking_project/CoworkingProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.coworking_project;
+package ch.zli.m223.ksh20.coworking_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
