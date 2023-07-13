5354793449c042285bb0ca306988a6cb4fa4693f
CuddlyBunion341 <daniel@bengl.cz>
2023-07-13 13:30:50 +0200

    created PasswordResetToken model

--- src/main/java/ch/zli/m223/ksh20/coworking_project/model/PasswordResetToken.java
+++ src/main/java/ch/zli/m223/ksh20/coworking_project/model/PasswordResetToken.java
@@ -1,4 +1,22 @@
 package ch.zli.m223.ksh20.coworking_project.model;
 
+import java.time.LocalDate;
+
 public interface PasswordResetToken {
+
+    public String getUuid();
+
+    public void setUuid(String uuid);
+
+    public String getToken();
+
+    public void setToken(String token);
+
+    public User getUser();
+
+    public void setUser(User user);
+
+    public LocalDate getExpireDate();
+
+    public void setExpireDate(LocalDate expireDate);
 }
